05caad99aed3e64b7868000d073af701bb75fb82
김동건 <kdk4270@naver.com>
2026-08-21 16:30:16 +0900

    first commit

---
+++ src/main/java/com/safeletter/smishing_checker/SmishingCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.safeletter.smishing_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmishingCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmishingCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safeletter/smishing_checker/SmishingCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safeletter.smishing_checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmishingCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
